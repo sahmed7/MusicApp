1b47f04eb147a0592182d3aa302f5f8c702a4a77
Dorjbat Mijiddorj <dmyvideo@gmail.com>
2021-04-10 15:23:53 -0500

    added user model

--- src/main/java/com/musicapp/demo/model/User.java
+++ src/main/java/com/musicapp/demo/model/User.java
@@ -1,4 +1,60 @@
 package com.musicapp.demo.model;
 
 public class User {
+    private Long Id;
+    private String userName;
+    private String emailAddress;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String userName, String emailAddress, String password) {
+        Id = id;
+        this.userName = userName;
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "Id=" + Id +
+                ", userName='" + userName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
