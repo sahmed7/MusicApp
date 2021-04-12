450f054cd9ddc4a579a9c3fd168e23fa4e285667
Dorjbat Mijiddorj <dmyvideo@gmail.com>
2021-04-12 11:42:57 -0500

    added GenreService

--- src/main/java/com/musicapp/demo/controller/GenreController.java
+++ src/main/java/com/musicapp/demo/controller/GenreController.java
@@ -1,8 +1,10 @@
 package com.musicapp.demo.controller;
 
+import com.musicapp.demo.model.Genre;
 import com.musicapp.demo.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,11 @@ public class GenreController {
     @GetMapping("/hello")
     public String helloWorld(){return "Hello World";}
 
+//    @GetMapping("/genres")
+//    public Genre getGenre(@PathVariable Long genreId){
+//        System.out.println("testing getGenre method");
+//        return genreService.getGenre(genreId);
+//    }
 
 
 
--- src/main/java/com/musicapp/demo/service/GenreService.java
+++ src/main/java/com/musicapp/demo/service/GenreService.java
@@ -1,4 +1,21 @@
 package com.musicapp.demo.service;
 
+import com.musicapp.demo.model.Genre;
+import com.musicapp.demo.repository.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GenreService {
+    private GenreRepository genreRepository;
+
+
+    @Autowired
+    public void setGenreRepository(GenreRepository genreRepository){
+        this.genreRepository = genreRepository;
+    }
+
+//    public Genre getGenre(Long genreId){
+//
+//    }
 }
