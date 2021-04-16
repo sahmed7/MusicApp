06d0079afea2856fa9fc369b952bd889879e41cd
Shaheer Ahmed <sahmed3721@gmail.com>
2021-04-16 09:28:02 -0500

    removed some excess comments

--- src/main/java/com/musicapp/demo/repository/UserRepository.java
+++ src/main/java/com/musicapp/demo/repository/UserRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository <User, Long> {
 
-    // to register
     boolean existsByEmailAddress(String userEmailAddress);
 
     User findById(int userId);
